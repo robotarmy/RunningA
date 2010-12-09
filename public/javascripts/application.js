d32567729acbe66203af04ce3c8edada8cad6d61
Curtis Schofield <github@robotarmyma.de>
2010-12-08 22:21:26 -0800

    json saved searches page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,45 +1,42 @@
 
-var tweet = {
+window.Tweet = function(tweet,location,_opts_) {
 
-  return {
-  }  
-}
-
-window.inject_tweet = function(tweet,location,_opts_ ) {
   var opts = _opts_ || {}
-  var el = jQuery('<span></span>')
-  el.addClass('tweet')
-  add_tweet(el,tweet)
-  if (opts.save) {
-    add_save(el,tweet)
-  }
-  if (location)
-    jQuery(location).append(el)
-}
-
-window.add_tweet = function(el,tweet) {
-  for(attr in tweet) {
-    var s = jQuery('<span></span>')
-    s.text(tweet[attr])
-    s.addClass(attr)
-    if (attr == 'id') {
-      el.attr('id',tweet[attr])
+  var add_tweet = function(el,tweet) {
+      for(attr in tweet) {
+        var s = jQuery('<span></span>')
+        s.text(tweet[attr])
+        s.addClass(attr)
+        if (attr == 'id') {
+          el.attr('id',tweet[attr])
+        }
+        el.append(s)
+      }
     }
-    el.append(s)
-  }
-}
 
-window.add_save = function(el,tweet) {
-  var save = jQuery('<a>save</a>')
-  save.click(function(){
-    jQuery.post("/tweets",
-                {tweet:{json:JSON.stringify(tweet)}},
-                function(data){
-                  var id = el.attr('id')
-                  jQuery('#'+id).fadeOut()
-                })
-  })
-  el.append(save)
+  var add_save = function(el,tweet) {
+      var save = jQuery('<a>save</a>')
+      save.click(function(){
+        jQuery.post("/tweets",
+                    {tweet:{json:JSON.stringify(tweet)}},
+                    function(data){
+                      var id = el.attr('id')
+                      jQuery('#'+id).fadeOut()
+                    })
+      })
+      el.append(save)
+  }
+  var inject_tweet = function() {
+    var el = jQuery('<span></span>')
+    el.addClass('tweet')
+    add_tweet(el,tweet)
+    if (opts.save) {
+      add_save(el,tweet)
+    }
+    if (location)
+      jQuery(location).append(el)
+  }();
+
 }
 
 window.load_tweets = function(opts) {
@@ -47,7 +44,7 @@ window.load_tweets = function(opts) {
                  function(data){
     if (data.results) {
       for(id in data.results) {
-        window.inject_tweet(data.results[id],'body',opts)
+        Tweet(data.results[id],'body',opts);
       }
     }
   })
